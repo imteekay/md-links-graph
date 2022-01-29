395aadd6e257b268accaa26c4a7834c1507b47e1
TK <leandrotk100@gmail.com>
2022-01-29 16:34:11 -0300

    fix test

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,36 +1,55 @@
 import { createGraph } from '..';
 import { Graph } from '../Graph';
 
+const excludedPages = [
+  '',
+  '/',
+  '/writings',
+  '/support',
+  '/rss.xml',
+  undefined,
+  null
+];
+
+function isValidLink(url: string) {
+  return (
+    !url.startsWith('https') &&
+    !url.startsWith('http') &&
+    !url.startsWith('www') &&
+    !excludedPages.includes(url)
+  );
+}
+
 describe('createGraph', () => {
   it('returns the links graph when receiving a list of posts', () => {
     const post1 = {
       url: '/series/typescript-learnings',
       title: 'TypeScript Learnings',
-      markdown: `- [Typescript Learnings: Interesting Types](/series/typescript-learnings/interesting-types)
+      content: `- [Typescript Learnings: Interesting Types](/series/typescript-learnings/interesting-types)
       - [Typescript Learnings: Interesting Types](/series/typescript-learnings/test)`
     };
 
     const post2 = {
       url: '/series/typescript-learnings/interesting-types',
       title: 'Interesting types',
-      markdown:
+      content:
         '[Typescript Learnings: Interesting Types](/series/typescript-learnings/test)'
     };
 
     const post3 = {
       url: '/series/typescript-learnings/test',
       title: 'Test',
-      markdown: ''
+      content: ''
     };
 
     const posts = [post1, post2, post3];
     const expectedGraph = new Graph(posts);
 
-    posts.forEach(({ url }) => expectedGraph.addNode(url));
+    posts.forEach((post) => expectedGraph.addNode(post));
     expectedGraph.addEdge(post1.url, post2.url);
     expectedGraph.addEdge(post1.url, post3.url);
     expectedGraph.addEdge(post2.url, post3.url);
 
-    expect(createGraph(posts)).toEqual(expectedGraph);
+    expect(createGraph(posts, isValidLink)).toEqual(expectedGraph);
   });
 });
